ae33e4ec7c78c5d6cc7d0602e2835b6d65a2543c
morgancfarrow90 <63519344+morgancfarrow90@users.noreply.github.com>
2020-07-17 11:17:43 -0400

    Done.

--- index.js
+++ index.js
@@ -27,3 +27,5 @@ function capitalize_Words(str)
 {
  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
 }
+
+const titleCased = tutorials.map(capitalize_Words);
