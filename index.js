db4f90074dc9469ef8be26eb4011ff20872a2b3c
rschlosser11 <rosalinschlosser@gmail.com>
2020-07-20 16:05:29 -0500

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(titleCase)
+}
+
+function titleCase(string) {
+  let arr = string.split(' ');
+  return arr.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ')
 }
