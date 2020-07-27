8bf1acc9aa9c716bba35967a58c322788407c350
webdevjscoder <cobianjuan19@gmail.com>
2020-07-27 15:51:08 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string => {
+    let words = string.split(' ');
+    let capitalizedWords =
+      words.map(word => word.charAt(0).toUpperCase() + word.slice(1) )
+    let response = capitalizedWords.join(' ')
+    return response
+  })
 }
