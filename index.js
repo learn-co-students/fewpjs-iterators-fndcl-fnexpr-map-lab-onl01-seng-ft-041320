76d543644e28de0f51dd4123e9b68b97b320ec69
DrewDalmedo <andrewdalmedo@gmail.com>
2020-07-29 12:39:30 -0400

    Done.

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+function titleCaseSentence(sentence) {
+  return sentence.split(' ').map( (word) => {
+    return word[0].toUpperCase() + word.slice(1);
+  }).join(' ')
+}
+
 const titleCased = () => {
-  return tutorials
+  return tutorials.map( (sentence) => {
+    return titleCaseSentence(sentence)
+  });
 }
